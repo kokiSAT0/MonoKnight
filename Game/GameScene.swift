cc31c0ddb858cdc95acfce5341c7817b2939292a
kokiSAT0 <kokisato0123@gmail.com>
2025-09-20 12:50:41 +0900

    Remove debug board coordinate labels

--- Game/GameScene.swift
+++ Game/GameScene.swift
@@ -109,15 +109,7 @@ class GameScene: SKScene {
                 addChild(node)
                 let point = GridPoint(x: x, y: y)
                 tileNodes[point] = node
-
-#if DEBUG
-                // デバッグ時は各マスの座標を赤文字で表示
-                let label = SKLabelNode(text: "\(x),\(y)")
-                label.fontSize = tileSize * 0.3
-                label.fontColor = .red
-                label.position = CGPoint(x: rect.midX, y: rect.midY - label.fontSize / 2)
-                addChild(label)
-#endif
+                // テストプレイ時の没入感を損なわないよう、デバッグ用の座標ラベルは描画しない
             }
         }
     }
